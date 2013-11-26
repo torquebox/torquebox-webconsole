5ba496f248d74c21876fca20148bde87532030da
Toby Crawley <toby@tcrawley.org>
2013-11-26 14:01:21 -0500

    Update to use TorqueBox.fetch.

--- lib/torquebox/webconsole.rb
+++ lib/torquebox/webconsole.rb
@@ -5,12 +5,11 @@ require 'torquebox'
 
 module TorqueBox
   class Webconsole
-    extend TorqueBox::Injectors
     class << self
-
+      
       def lookup_runtime(name)
-        service_registry = inject('service-registry')
-        unit = inject('deployment-unit')
+        service_registry = TorqueBox.fetch('service-registry')
+        unit = TorqueBox.fetch('deployment-unit')
         
         service_name = org.torquebox.core.as.CoreServices.runtimePoolName(unit, name)
         service_controller = service_registry.get_service(service_name)
@@ -27,13 +26,13 @@ module TorqueBox
       end
 
       def list_runtimes
-        prefix = inject('deployment-unit').
+        prefix = TorqueBox.fetch('deployment-unit').
           service_name.
           append(org.torquebox.core.as.CoreServices::RUNTIME).
           append("pool").
           canonical_name
         
-        inject('service-registry').service_names.to_a.map do |x|
+        TorqueBox.fetch('service-registry').service_names.to_a.map do |x|
           $1 if x.canonical_name =~ %r{#{prefix}\.([^.]+)$}
         end.reject(&:nil?)
       end
